40401cb7ceabc95e0270e74e5e61994d78e42381
AmanSingh494 <amanforwork1@gmail.com>
2024-07-10 13:08:18 +0530

    Created this branch to make a way to take multiple experiences input from the user

---
+++ src/components/ExperiencesInput.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const InputContainer = styled.div`
+    
+`
+const ExperiencesInput = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ExperiencesInput
